7a429561c0e0a0d638809fc063aa490b4cf5b19a
BloodyCrown <3170989534@qq.com>
2025-02-19 23:52:06 +0800

    feat(main): 添加图片缓存和快捷键功能
    - 实现了LRU缓存机制，用于缓存图片
    - 添加了快捷键配置，支持自定义快捷键- 新增了图片移动功能- 优化了图片加载逻辑，支持从缓存中加载图片- 重构了部分代码，提高了可维护性

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, shutil, json
 from natsort import natsorted
 
 from PySide6.QtWidgets import (
@@ -9,6 +9,34 @@ from PySide6.QtGui import QAction, QPixmap, QImage, QKeyEvent, QResizeEvent
 from PySide6.QtCore import Qt, QTimer
 from PIL import Image, ImageSequence
 import os
+from collections import OrderedDict
+
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key, value) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+    def has(self, key) -> bool:
+        return key in self.cache
+
+    def remove(self, key) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
 
 
 class ImageViewer(QMainWindow):
@@ -16,6 +44,7 @@ class ImageViewer(QMainWindow):
         super().__init__()
         self.setWindowTitle("Simple Image Viewer")
         self.setMinimumSize(800, 600)
+        self.config = self.load_config()
 
         # 初始化变量
         self.current_image_path = None
@@ -24,6 +53,9 @@ class ImageViewer(QMainWindow):
         self.rotation_angle = 0
         self.is_fullscreen = False
         self.original_pixmap = None  # 保存原始图片
+        # 缓存半径
+        self.cache_radius = self.config["cache_radius"]
+        self.image_cache = LRUCache(self.cache_radius * 2 + 1)
 
         # GIF支持
         self.gif_frames = []
@@ -33,6 +65,12 @@ class ImageViewer(QMainWindow):
 
         # 创建界面
         self.create_ui()
+
+    def load_config(self):
+        path = os.path.join(os.path.dirname(__file__), "config.json")
+        with open(path, 'r') as f:
+            config = json.load(f)
+        return config
 
     def create_ui(self):
         # 主窗口部件
@@ -94,26 +132,46 @@ class ImageViewer(QMainWindow):
             ]
             self.image_files = natsorted(self.image_files)
             self.current_image_index = self.image_files.index(file)
+            self.init_image_cache(self.image_files, self.current_image_index)
             self.load_image(file)
+
+    def load_images_to_cache(self, paths: list):
+        for path in paths:
+            img = Image.open(path)
+            self.image_cache.put(path, img)
+
+    def init_image_cache(self, image_files, current_index):
+        if len(image_files) < self.cache_radius * 2 + 1:
+            self.load_images_to_cache(image_files)
+        else:
+            # 不支持越界索引
+            left_paths = [image_files[index % len(image_files)]
+                          for index in range(current_index + 1, self.cache_radius + current_index + 1)]
+            self.load_images_to_cache(left_paths)
+            # 支持负索引
+            right_paths = [image_files[index] for index in range(current_index - self.cache_radius, current_index)]
+            self.load_images_to_cache(right_paths)
 
     def load_image(self, path):
         # 停止GIF动画
         self.gif_timer.stop()
+        if self.image_cache.has(path):
+            img = self.image_cache.get(path)
+        else:
+            img = Image.open(path)
 
-        # 读取图片
-        with Image.open(path) as img:
-            if img.format == 'GIF':
-                # 处理动态GIF
-                self.gif_frames = []
-                for frame in ImageSequence.Iterator(img):
-                    self.gif_frames.append(frame.copy().convert("RGBA"))
-                self.current_gif_frame = 0
-                self.start_gif_animation()
-            else:
-                # 静态图片
-                img = img.convert("RGBA")
-                self.display_pil_image(img)
-
+        if img.format == 'GIF':
+            # 处理动态GIF
+            self.gif_frames = []
+            for frame in ImageSequence.Iterator(img):
+                self.gif_frames.append(frame.copy().convert("RGBA"))
+            self.current_gif_frame = 0
+            self.start_gif_animation()
+        else:
+            # 静态图片
+            img = img.convert("RGBA")
+            self.display_pil_image(img)
+        img.close()
         # 更新状态栏信息
         self.update_status_bar(path)
 
@@ -187,12 +245,20 @@ class ImageViewer(QMainWindow):
         self.fit_image_to_window()
         super().resizeEvent(event)
 
+    def move_image(self, src, dest):
+        self.image_files.remove(src)
+        self.image_cache.remove(src)
+        shutil.move(src, dest)
+
     def prev_image(self):
         if self.current_image_index == 0:
             self.current_image_index = len(self.image_files) - 1
         else:
             self.current_image_index -= 1
         self.load_image(self.image_files[self.current_image_index])
+        next_cache_index = self.current_image_index - self.cache_radius
+        next_cache_index = next_cache_index if next_cache_index > 0 else next_cache_index + len(self.image_files)
+        self.load_images_to_cache([self.image_files[next_cache_index]])
 
     def next_image(self):
         if self.current_image_index == len(self.image_files) - 1:
@@ -200,6 +266,8 @@ class ImageViewer(QMainWindow):
         else:
             self.current_image_index += 1
         self.load_image(self.image_files[self.current_image_index])
+        next_cache_index = (self.current_image_index + self.cache_radius) % len(self.image_files)
+        self.load_images_to_cache([self.image_files[next_cache_index]])
 
     def rotate_image(self, angle):
         self.rotation_angle += angle
@@ -221,31 +289,27 @@ class ImageViewer(QMainWindow):
     def keyPressEvent(self, event: QKeyEvent):
         # 快捷键处理
         key = event.key()
-        # key = event.key()
-        # modifiers = event.modifiers()
-        #
-        # if modifiers == Qt.ControlModifier:
-        #     if key == Qt.Key_1:
-        #         self.move_image_to_folder(1)
-        #     elif key == Qt.Key_2:
-        #         self.move_image_to_folder(2)
-        #     elif key == Qt.Key_3:
-        #         self.move_image_to_folder(3)
-        if key == Qt.Key.Key_Left:
-            self.prev_image()
-        elif key == Qt.Key.Key_Right:
-            self.next_image()
-        elif key == Qt.Key.Key_R:
-            self.rotate_image(90)  # 顺时针旋转
-        elif key == Qt.Key.Key_L:
-            self.rotate_image(-90)  # 逆时针旋转
-        elif key == Qt.Key.Key_F:
-            self.toggle_fullscreen()
-        elif key == Qt.Key.Key_Escape:
-            if self.is_fullscreen:
-                self.toggle_fullscreen()
-        else:
-            super().keyPressEvent(event)
+        modifiers = event.modifiers()
+        shortcut = self.config["shortcut"]
+        for obj in shortcut:
+            if obj["key"] == key and obj["modifiers"] == modifiers:
+                if obj["action"] == "prev_image":
+                    self.prev_image()
+                elif obj["action"] == "next_image":
+                    self.next_image()
+                elif obj["action"] == "rotate_right":
+                    self.rotate_image(90)  # 顺时针旋转
+                elif obj["action"] == "rotate_left":
+                    self.rotate_image(-90)  # 逆时针旋转
+                elif obj["action"] == "enter_fullscreen":
+                    self.toggle_fullscreen()
+                elif obj["action"] == "exit_fullscreen":
+                    if self.is_fullscreen:
+                        self.toggle_fullscreen()
+                elif obj["action"] == "move":
+                    self.move_image(self.current_image_path, obj["dir"])
+                else:
+                    super().keyPressEvent(event)
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,4 @@
+from PySide6.QtCore import Qt, QTimer
+import main
+if __name__ == '__main__':
+    print(dir(main.ImageViewer()))
